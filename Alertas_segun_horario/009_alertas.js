65e0d27a02840935fbb69aba213d983927e8deeb
FranikyOcho <131266949+FranikyOcho@users.noreply.github.com>
2023-04-21 15:35:47 +0200

    Delete Alertas_segun_horario directory

--- Alertas_segun_horario/009_alertas.js
+++
@@ -1,9 +0,0 @@
-// Obtiene la hora actual
-var hora = new Date().getHours();
-
-// Muestra una alerta flotante dependiendo de la hora
-if (hora >= 0 && hora < 12) {
-	alert("Buenos días!");
-} else {
-	alert("Buenas noches!");
-}
